096d80f2a7d6b83e341aea4322990d5fdd894bfe
Andreas Anastasiou <aanast01@ucy.ac.cy>
2023-11-01 22:21:23 +0200

    using functions

--- main.go
+++ main.go
@@ -13,60 +13,11 @@ func main() {
 	var bookings_tickets []uint
 
 	for remainingTickets > 0 {
-		// Print welcome message
-		fmt.Printf("Welcome to %v booking application\n", conferenceName)
-		fmt.Printf("We have %v remaining tickets\n", remainingTickets)
-		fmt.Println("Get your tickets here to attend")
-
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
+		// greet user
+		greetUser(conferenceName, remainingTickets)
 
 		// ask user for their name
-		var isValidName = false
-		for !isValidName {
-			fmt.Println("Enter your first name: ")
-			fmt.Scan(&firstName)
-			isValidName = len(firstName) >= 2
-			if !isValidName {
-				fmt.Printf("Invalid first name. First name must be at least 2 characters\n")
-			}
-		}
-
-		isValidName = false
-		for !isValidName {
-			fmt.Println("Enter your last name: ")
-			fmt.Scan(&lastName)
-			isValidName = len(lastName) >= 2
-			if !isValidName {
-				fmt.Printf("Invalid last name. Last name must be at least 2 characters\n")
-			}
-		}
-
-		var isValidEmail = false
-		for !isValidName {
-			fmt.Println("Enter your email: ")
-			fmt.Scan(&email)
-			isValidEmail = strings.Contains(email, "@")
-			if !isValidEmail {
-				fmt.Printf("Invalid email. Email must contain the @ character\n")
-			}
-		}
-
-		var isValidTicketNumber = false
-		for !isValidTicketNumber {
-			fmt.Println("Enter how many tickets you want: ")
-			fmt.Scan(&userTickets)
-			isValidTicketNumber = userTickets > 0 && userTickets < remainingTickets
-			if !isValidTicketNumber {
-				fmt.Printf("Invalid number of tickets. Ticket number must be between 1 and %v\n", remainingTickets)
-			}
-		}
-
-		bookings_names = append(bookings_names, firstName+" "+lastName)
-		bookings_tickets = append(bookings_tickets, userTickets)
-		remainingTickets = remainingTickets - userTickets
+		var email = getUserInput(&bookings_names, &bookings_tickets, &remainingTickets)
 
 		fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email at %v.\n", bookings_names[len(bookings_names)-1], bookings_tickets[len(bookings_tickets)-1], email)
 		fmt.Printf("Remaining tickets for %v: %v\n\n", conferenceName, remainingTickets)
@@ -74,9 +25,75 @@ func main() {
 
 	fmt.Printf("Conference %v is fully booked. Thank you!!!\n\n", conferenceName)
 
+	// print all reservations
+	printReservations(&bookings_names, &bookings_tickets)
+}
+
+func greetUser(conferenceName string, remainingTickets uint) {
+	// Print welcome message
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have %v remaining tickets\n", remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+func getUserInput(bookings_names *[]string, bookings_tickets *[]uint, remainingTickets *uint) string {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	var isValidName = false
+	for !isValidName {
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+		isValidName = len(firstName) >= 2
+		if !isValidName {
+			fmt.Printf("Invalid first name. First name must be at least 2 characters\n")
+		}
+	}
+
+	isValidName = false
+	for !isValidName {
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+		isValidName = len(lastName) >= 2
+		if !isValidName {
+			fmt.Printf("Invalid last name. Last name must be at least 2 characters\n")
+		}
+	}
+
+	var isValidEmail = false
+	for !isValidEmail {
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+		isValidEmail = strings.Contains(email, "@")
+		if !isValidEmail {
+			fmt.Printf("Invalid email. Email must contain the @ character\n")
+		}
+	}
+
+	var isValidTicketNumber = false
+	for !isValidTicketNumber {
+		fmt.Println("Enter how many tickets you want: ")
+		fmt.Scan(&userTickets)
+		isValidTicketNumber = userTickets > 0 && userTickets <= *remainingTickets
+		if !isValidTicketNumber {
+			fmt.Printf("Invalid number of tickets. Ticket number must be between 1 and %v\n", *remainingTickets)
+		}
+	}
+
+	*bookings_names = append(*bookings_names, firstName+" "+lastName)
+	*bookings_tickets = append(*bookings_tickets, userTickets)
+	*remainingTickets = *remainingTickets - userTickets
+
+	return email
+}
+
+func printReservations(bookings_names *[]string, bookings_tickets *[]uint) {
 	fmt.Printf("----- All Reservations -----\n")
 
-	for index, booking := range bookings_names {
-		fmt.Printf("Reservation %v:\nName: %v    Tickets: %v\n", index+1, strings.Fields(booking)[0], bookings_tickets[index])
+	var tickets = *bookings_tickets
+	for index, booking := range *bookings_names {
+		fmt.Printf("Reservation %v:\nName: %v    Tickets: %v\n", index+1, strings.Fields(booking)[0], tickets[index])
 	}
 }
